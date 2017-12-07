03a3374884b5a516b7d53d2ee526b8370c549bd0
Dennis Stücken <dstuecken@me.com>
2017-12-07 16:12:14 +0100

    properly waiting until mysql is launched

--- docker/app/bin/04-start-daemons.sh
+++ docker/app/bin/04-start-daemons.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
 # Wait until mysql is started and all dumps are imported
-echo "Waiting 180 secs.."
-sleep 180
+cat << EOF > /tmp/wait_for_mysql.php
+<?php
+\$connected = false;
+while(!\$connected) {
+    try{
+        \$dbh = new pdo(
+            'mysql:host=mysql1:3306;dbname=' . getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'),
+            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+        );
+        \$connected = true;
+    }
+    catch(PDOException \$ex){
+        error_log("Could not connect to MySQL");
+        error_log(\$ex->getMessage());
+        error_log("Waiting for MySQL Connection.");
+        sleep(5);
+    }
+}
+EOF
+php /tmp/wait_for_mysql.php
+rm /tmp/wait_for_mysql.php
+
+wait 5
 
 echo "Starting Table daemon.."
 /usr/bin/php /project/application/bin/cli.php NewQueue Table --name=touchTable &
