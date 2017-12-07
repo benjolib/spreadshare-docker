8af0fd9baf5cc2b4c3d4cd735d5a316cf0ee3f70
Dennis Stücken <dstuecken@me.com>
2017-12-07 15:32:28 +0100

    waiting 30 secs before launching daemons that are relying on mysql

--- docker/app/bin/04-start-daemons.sh
+++ docker/app/bin/04-start-daemons.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Wait until mysql is started
+echo "Waiting 30 secs.."
+sleep 30
+
 echo "Starting Table daemon.."
 /usr/bin/php /project/application/bin/cli.php NewQueue Table --name=touchTable &
 
