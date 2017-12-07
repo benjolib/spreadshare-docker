eecb964b99eeb861a9bd7142795b9f92c530ea6b
Dennis Stücken <dstuecken@me.com>
2017-12-07 16:18:16 +0100

    checking for last table wallet in mysql waiting script

--- docker/app/bin/04-start-daemons.sh
+++ docker/app/bin/04-start-daemons.sh
@@ -10,12 +10,11 @@ while(!\$connected) {
             'mysql:host=mysql1:3306;dbname=' . getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'),
             array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
         );
+        \$dbh->query('SELECT * from wallet;');
         \$connected = true;
     }
     catch(PDOException \$ex){
-        error_log("Could not connect to MySQL");
-        error_log(\$ex->getMessage());
-        error_log("Waiting for MySQL Connection.");
+        error_log("Waiting for MySQL to be up and running...");
         sleep(5);
     }
 }
